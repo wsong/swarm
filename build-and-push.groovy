00c89af6fa2291506c77b949fb6ddb5de8056a32
Daniel Hiltgen <daniel.hiltgen@docker.com>
2018-01-10 16:04:44 -0800

    Add IBM P support

--- build-and-push.groovy
+++ build-and-push.groovy
@@ -36,4 +36,8 @@ parallel(
     s390xlinux: { wrappedNode(label: 's390x-ubuntu-1604') {
         buildAndPush()
     }}
+
+    ppc64le: { wrappedNode(label: 'ppc64le-ubuntu-1604') {
+        buildAndPush()
+    }}
 )
